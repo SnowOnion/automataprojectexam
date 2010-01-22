7e1016f65901d752539c675fec1f20ec78653a78
newleaf.lu <newleaf.lu@55acb992-dcb3-11de-9872-ef835a306e00>
2010-01-22 12:18:07 +0000

    add comments

--- trunk/41DFAAlgorithm2/src/automata/Transitions.java
+++ trunk/41DFAAlgorithm2/src/automata/Transitions.java
@@ -4,15 +4,27 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * 一个状态所关联的迁移集合。
+ * @author Leaf
+ *
+ */
 public class Transitions {
 //------------------------------------------------------------------------------
-	// Add a new transition.
+	/**
+	 * 添加新的迁移。
+	 * @param s 迁移对象
+	 */
 	public void addTransition(Transition s) {
 		trans.add(s);
 		conditions.addAll(s.conditions());
 	}
 	
 	// Remove an exist transition, and mark it as invalid.
+	/**
+	 * 删除迁移
+	 * @param s 要删除的迁移对象
+	 */
 	public void removeTransition(Transition s) {
 		Transition t;
 		Iterator<Transition> it = trans.iterator();
@@ -27,21 +39,37 @@ public class Transitions {
 	}
 	
 	// Return whether there exist a transition accepting 'c'.
+	/**
+	 * 返回是否存在条件为 c 的迁移。
+	 * @param c 迁移条件（字符）
+	 * @return true：存在；false：不存在。
+	 */
 	public boolean containsCond(char c) {
 		return conditions.contains(c);
 	}
 	
+	/**
+	 * 返回所有的迁移条件。
+	 * @return 迁移条件集合
+	 */
 	public HashSet<Character> conditions() {
 		return conditions;
 	}
 	
 	// Return whether this set of transitions is empty.
+	/**
+	 * 判断本迁移集合是否为空。
+	 * @return true：集合为空；false：集合非空。
+	 */
 	public boolean isEmpty() {
 		return trans.isEmpty();
 	}
 	
 	
 	// Remove all invalid transitions.
+	/**
+	 * 删除所有无效迁移。
+	 */
 	public void cleanUp() {
 		Transition t;
 		Iterator<Transition> it = trans.iterator();
@@ -56,6 +84,10 @@ public class Transitions {
 	
 	// Dispose all transitions within, probably for that the state owning these
 	// transitions has been deleted.
+	/**
+	 * 销毁此迁移集合。<br>
+	 * 当迁移所关联的状态被删除时执行此函数。
+	 */
 	public void dispose() {
 		Transition t;
 		Iterator<Transition> it = trans.iterator();
@@ -66,6 +98,9 @@ public class Transitions {
 	}
 	
 	// Update the conditions when some transition is removed
+	/**
+	 * 更新本集合所包含的迁移条件。
+	 */
 	protected void updateConditions() {
 		Iterator<Transition> t = trans.iterator();
 		while (t.hasNext()) {
