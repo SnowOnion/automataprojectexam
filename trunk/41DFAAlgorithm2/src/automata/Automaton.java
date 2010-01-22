88d2ab705cd0f84fdde23215893ded98a6df526e
newleaf.lu <newleaf.lu@55acb992-dcb3-11de-9872-ef835a306e00>
2010-01-22 12:17:41 +0000

    add comments

--- trunk/41DFAAlgorithm2/src/automata/Automaton.java
+++ trunk/41DFAAlgorithm2/src/automata/Automaton.java
@@ -9,12 +9,20 @@ import java.util.Set;
 
 public abstract class Automaton {
 //------Used by construction procedure------------------------------------------
+	/**
+	 * 添加状态。
+	 * @param label 状态名
+	 */
 	public void addState(String label) {
 		states.add(new State(label));
 		label2num.put(label, size);
 		++size;
 	}
 	
+	/**
+	 * 添加或设置初始状态。
+	 * @param label 状态名
+	 */
 	public void setStartState(String label) {
 		if (!label2num.containsKey(label))
 			addState(label);
@@ -22,6 +30,10 @@ public abstract class Automaton {
 		modified = true;
 	}
 	
+	/**
+	 * 添加或设置结束状态。
+	 * @param label 状态名
+	 */
 	public void setFinalState(String label) {
 		if (!label2num.containsKey(label))
 			addState(label);
@@ -29,23 +41,52 @@ public abstract class Automaton {
 		modified = true;
 	}
 	
+	/**
+	 * 添加迁移。
+	 * @param from 源状态对象
+	 * @param cond 迁移条件
+	 * @param to 目的状态对象
+	 */
 	public abstract void addTransition(State from, HashSet<Character> cond, State to);
 	
 //------Utilities, internal-----------------------------------------------------
+	/**
+	 * 返回状态名所对应的状态对象。
+	 * @param label 状态名
+	 * @return 状态对象
+	 */
 	protected State stateOfLabel(String label) {
 		return states.get(label2num.get(label));
 	}
+	/**
+	 * 返回指定状态的名称。
+	 * @param num 状态标号
+	 * @return 状态名
+	 */
 	protected State stateOfNum(int num) {
 		return states.get(num);
 	}
+	/**
+	 * 返回指定状态的标号。
+	 * @param num 状态名称
+	 * @return 状态标号
+	 */
 	protected int numOfState(State s) {
 		return label2num.get(s.label());
 	}
 	
 //------Used by algorithms, internal--------------------------------------------
+	/**
+	 * 删除状态。
+	 * @param stateNum 状态标号
+	 */
 	protected void removeState(int stateNum) {
 		removeState(states.get(stateNum));
 	}
+	/**
+	 * 删除状态。
+	 * @param stateNum 状态对象
+	 */
 	protected void removeState(State state) {
 		Set<State> s;
 		Iterator<State> it;
@@ -86,11 +127,32 @@ public abstract class Automaton {
 	
 	
 //------Data member-------------------------------------------------------------
+	/**
+	 * 自动机所含状态数
+	 */
 	protected int size = 0;
+	/**
+	 * 所有状态集合
+	 */
 	protected ArrayList<State> states = new ArrayList<State>();
+	/**
+	 * 状态名->标号映射
+	 */
 	protected HashMap<String, Integer> label2num = new HashMap<String, Integer>();
+	/**
+	 * 初始状态
+	 */
 	protected State startState;
+	/**
+	 * 终结状态集合
+	 */
 	protected HashSet<State> finalStates = new HashSet<State>();
+	/**
+	 * 字母表
+	 */
 	protected HashSet<Character> alphabet = new HashSet<Character>();
+	/**
+	 * 自动机是否在上次化简后被修改
+	 */
 	protected boolean modified = false;
 }
